44d2fd269b6928996b3f5e247de3e51e500e9ce5
Paul Mayer <paul@paul-mayer.eu>
2022-04-20 09:58:09 +0200

    try wasm compile

---
+++ web/README.md
@@ -0,0 +1,4 @@
+#dev
+```bash
+cargo watch -i .gitignore -i "pkg/*" -s "wasm-pack build --target web --release" 
+```
---
+++ web/src/lib.rs
@@ -0,0 +1,36 @@
+mod utils;
+use client;
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+}
+
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen]
+pub async fn greet() {
+    console_log!("Hello {}!", "WASM");
+    client::session::start_session().await;
+
+}
+
+#[wasm_bindgen(start)]
+pub fn main() {
+    greet();
+    //session::start_session();
+}
